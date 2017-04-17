10096c12ce5f3c88b5ea72311fe6bb5d32007035
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-04-17 17:39:32 -0400

    fade in about me lists using homemade function

---
+++ assets/js/scripts.js
@@ -0,0 +1,24 @@
+$(window).scroll(function () {
+    $('.animation-enterance').each(function () {
+        var imagePos = $(this).offset().top;
+        var imageHeight = $(this).height();
+        var topOfWindow = $(window).scrollTop();
+        var bottomOfWindow = $(window).height() + topOfWindow;
+        var twentyPercent = $(window).height() * .2;
+
+
+        if ( bottomOfWindow - twentyPercent > imagePos + imageHeight && topOfWindow + twentyPercent < imagePos + imageHeight) {
+            // $(this).addClass("fadeIn");
+            // $(this).removeClass("fadeOut");
+            $(this).css({'opacity': 1 })
+        } else if ( bottomOfWindow > imagePos && bottomOfWindow - twentyPercent < imagePos ) {
+            // $(this).removeClass("fadeIn");
+            // $(this).addClass("fadeOut");
+            var opac = Math.sin( ( bottomOfWindow - imagePos ) / twentyPercent )
+            $(this).css({'opacity':opac})
+        } else if ( topOfWindow < imagePos && topOfWindow + twentyPercent > imagePos) {
+            var opac = Math.sin( (imagePos - topOfWindow) / twentyPercent )
+            $(this).css({'opacity':opac});
+        }
+    });
+});
