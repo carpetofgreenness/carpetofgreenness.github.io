dd15ce57a8f1b0db9830d74df79450e0ec64ccc1
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-04-20 13:04:15 -0400

    comment js file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,6 @@
+//this js makes anything with the class "animation-enterence" gradually fade on scroll
+//in the top and bottom 20% of the screen
+
 $(window).scroll(function () {
     $('.animation-enterance').each(function () {
         var imagePos = $(this).offset().top;
@@ -6,23 +9,28 @@ $(window).scroll(function () {
         var bottomOfWindow = $(window).height() + topOfWindow;
         var twentyPercent = $(window).height() * .2;
 
-
+        //check if the item is in the middle 60%
         if ( bottomOfWindow - twentyPercent > imagePos + imageHeight && topOfWindow + twentyPercent < imagePos + imageHeight) {
-            // $(this).addClass("fadeIn");
-            // $(this).removeClass("fadeOut");
+            //not faded
             $(this).css({'opacity': 1 })
         } else if ( bottomOfWindow > imagePos && bottomOfWindow - twentyPercent < imagePos ) {
-            // $(this).removeClass("fadeIn");
-            // $(this).addClass("fadeOut");
+            //this is the bottom 20%
+            //on a scale of 0-1, find out where item is relative to bottom of window
+            //this will determine opacity
+            //if it is closer to the bottom, it will have opacity closer to 0
+            //the sin function makes it look more natural
             var opac = Math.sin( ( bottomOfWindow - imagePos ) / twentyPercent )
             $(this).css({'opacity':opac})
         } else if ( topOfWindow < imagePos && topOfWindow + twentyPercent > imagePos) {
+            //same logic as above, except with the top 20%
+            //if it is closer to the top, opacity is closer to 0
             var opac = Math.sin( (imagePos - topOfWindow) / twentyPercent )
             $(this).css({'opacity':opac});
         }
     });
 });
 
+//load the page with opacity already showing
 $(document).ready(function() {
         $('.animation-enterance').each(function () {
         var imagePos = $(this).offset().top;
